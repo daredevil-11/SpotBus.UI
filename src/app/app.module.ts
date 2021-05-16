3b01682e3c2c9a3760669308f067d443f2c84dc4
daredevil <td3331@gmail.com>
2021-05-17 01:09:06 +0530

    added dashboard component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+/** Compnents */
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+/** UI Components */
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
